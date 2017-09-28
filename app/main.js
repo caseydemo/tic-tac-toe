b79a7081b9d99b2236ef9264708ae4242463982e
caseydemo <cdemattina@gmail.com>
2017-09-28 14:24:57 -0400

    pretty well finished, now I will try to push this to heroku

--- app/main.js
+++ app/main.js
@@ -2,6 +2,8 @@ var restartButton = document.getElementById('restartButton');
 var spaces = document.getElementsByClassName('space');
 var symbols = ['O', 'X'];
 var turn = 0;
+var notification = document.getElementById('notification');
+var winnerMessage = document.getElementById('winnerMessage');
 
 
 // waits for page to load before doing stuff
@@ -19,6 +21,8 @@ function startGame(){
 
 	// remove the winner notification make sure div is hidden
 	// add click events on squares
+	winnerMessage.innerHTML = '';
+	notification.style.display = "none";
 
 	for(var i=0; i<spaces.length; i++){
 		spaces[i].innerHTML = '';
@@ -30,20 +34,38 @@ function takeSpace() {
 
 	console.log(wins);
 	turn++;
-	this.innerHTML = symbols[turn % 2];
+	var currentPlater = symbols[turn % 2];
+	this.innerHTML = currentPlater;
 	this.removeEventListener("click", takeSpace);
 	
 	for(var i=0; i<wins.length; i++){
-	if (checkForWin(wins[i]){
-		alert("woo hoo!");
+	if (checkForWin(wins[i])){
+		// alert("woo hoo!");
+
+		// no more clicking!
+		for(var j=0; j<spaces.length; j++){
+			spaces[j].removeEventListener("click", takeSpace);
 		}
+
+		// Notify the player of success
+		notification.style.display = "block";
+		winnerMessage.innerHTML = "Yay! " + currentPlater + " won!";
+		}
+		else{
+			if(turn ==9){
+				notification.style.display = "block";
+				winnerMessage.innerHTML="bummer you both lose";
+			}
+		}
+		
 	}
+	
 }
 
-// function checkForWin(winArray){
+function checkForWin(winArray){
 
-// 	return	spaces[winArray[0]] !== '' && 
-// 	spaces[winArray[0]] === spaces[winArray[1]] && 
-// 	spaces[winArray[0]] === spaces[winArray[2]];
+	return	spaces[winArray[0]].innerHTML !== '' && 
+	spaces[winArray[0]].innerHTML === spaces[winArray[1]].innerHTML && 
+	spaces[winArray[0]].innerHTML === spaces[winArray[2]].innerHTML;
 	
-// }
+}
